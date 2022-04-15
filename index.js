d33d7689fc9cf823a03d25477cdf6bcf5eea5228
Sean Sarokas <sps4462@gmail.com>
2022-04-15 06:26:44 -0400

    commit tree 004daf2b102a64b56f158a1e7b0808b42e2969f4

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+let messages = [];
+function writeCards(name, event){
+    for(let i=0; i<name.length; i++){
+       messages[i] = `Thank you, ${name[i]}, for the wonderful ${event} gift!`
+    }
+    return messages;
+}
+
+function countDown(integer){
+    for(let i=integer; i>=0; i--){
+        console.log(i);
+    }
+}
